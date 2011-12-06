e9520540db26c40c80cc699a386fc73d31c94524
Olivier Mansion <olivier.mansion@shiningpanda.com>
2011-12-06 19:08:21 +0100

    modify test script

--- tests/run.py
+++ tests/run.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
-import nose
 
 folder = os.path.dirname(os.path.abspath(__file__))
 base = os.path.dirname(folder)
 
 sys.path.insert(0, base)
-sys.path.insert(1, os.path.join('djangotutorial'))
+sys.path.insert(0, os.path.join('djangotutorial'))
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 
 os.chdir(folder)
 
+import nose
 nose.main()
