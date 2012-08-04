1f63f03ad323b6fe2a6fdc0d279e486872c61000
Olivier Mansion <olivier.mansion@shiningpanda.com>
2012-08-04 12:57:33 +0200

    naming coherency

--- polls/tests.py
+++ polls/tests.py
@@ -8,23 +8,23 @@ class BaseTestCase(LiveServerTestCase):
     
     @classmethod
     def setUpClass(cls):
-        cls.selenium = WebDriver()
+        cls.driver = WebDriver()
         super(BaseTestCase, cls).setUpClass()
 
     @classmethod
     def tearDownClass(cls):
         super(BaseTestCase, cls).tearDownClass()
-        cls.selenium.quit()
+        cls.driver.quit()
 
 class AdminTestCase(BaseTestCase):
 
     def test_login(self):
         # Open the administration page
-        self.selenium.get(self.live_server_url + '/admin/')
+        self.driver.get(self.live_server_url + '/admin/')
         # Enter the name of the user
-        self.selenium.find_element_by_id('id_username').send_keys('admin')
+        self.driver.find_element_by_id('id_username').send_keys('admin')
         # Get the password input
-        password = self.selenium.find_element_by_id('id_password')
+        password = self.driver.find_element_by_id('id_password')
         # Type password
         password.send_keys('admin')
         # Submit the form
@@ -32,21 +32,21 @@ class AdminTestCase(BaseTestCase):
         # Load page
         time.sleep(2)
         # Check that welcomed
-        self.assertTrue(self.selenium.find_element_by_id('user-tools').text.startswith('Welcome'))
+        self.assertTrue(self.driver.find_element_by_id('user-tools').text.startswith('Welcome'))
 
 class PollsTestCase(BaseTestCase):
 
     def test_vote(self):
         # Open the polls page
-        self.selenium.get(self.live_server_url + '/polls/')
+        self.driver.get(self.live_server_url + '/polls/')
         # Get the link to the poll "ShiningPanda is a..."
-        poll = self.selenium.find_element_by_link_text('ShiningPanda is a...')
+        poll = self.driver.find_element_by_link_text('ShiningPanda is a...')
         # Click on the link
         poll.click()
         # Load page
         time.sleep(2)
         # Get the available choices
-        choices = self.selenium.find_elements_by_name('choice')
+        choices = self.driver.find_elements_by_name('choice')
         # Check that only three choices: "Hosted CI service?", "Consulting firm?" and "Both!"
         self.assertEquals(3, len(choices))
         # Select "Both!"
@@ -54,7 +54,7 @@ class PollsTestCase(BaseTestCase):
         # Submit the form
         choices[2].submit()
         # Get the poll results
-        lis = self.selenium.find_elements_by_tag_name('li')
+        lis = self.driver.find_elements_by_tag_name('li')
         # Check that results are displayed for each choices (three choices)
         self.assertEquals(3, len(lis))
         # Check that "Hosted CI service?" has no votes
